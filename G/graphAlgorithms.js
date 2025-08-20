dad54594120f5683768be2517aca25c89e4590ae
simcity4006 <foysalsimcity4771@gmail.com>
2025-08-20 08:35:51 +0600

    graphAlgorithm some change

--- G/graphAlgorithms.js
+++ G/graphAlgorithms.js
@@ -1,1 +1,23 @@
 // bfs dfs dijkstra's algorithms and other graph traversal techniques
+// Graph representation (Adjacency List)
+const graph = {
+  A: ['B', 'C'],
+  B: ['D', 'F'],
+  C: ['F'],
+  D: [],
+  E: ['F'],
+  F: [],
+};
+function bfs(start) {
+  let visited = new Set();
+  let queue = [start];
+  while (queue.length > 0) {
+    let node = queue.shift();
+    if (!visited.has(node)) {
+      console.log(node);
+      visited.add(node);
+      queue.push(...graph[node]);
+    }
+  }
+}
+bfs('A');
